f3e71bb09a775ed3fca0ee68f399ba338d326ff0
Vishal22598 <vishalgupta22598@gmail.com>
2026-02-04 18:31:52 +0530

    i create a demo spring boot application with dependencies web, dataJpa and h2 to create application with two databases

---
+++ src/main/java/com/demo/Demo2DbApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2DbApplication.class, args);
+	}
+
+}
